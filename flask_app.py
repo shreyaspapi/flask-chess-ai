3192d69c251df863685b2057217fba787636512e
Tunap <tunappaul@gmail.com>
2019-10-17 16:45:37 +0530

    updates

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from chess_engine import *
+import os
 
 app = Flask(__name__)
 
@@ -26,5 +27,5 @@ def test_get(tester):
 
 if __name__ == '__main__':
     app.debug = False
-    port = int(5000)
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, threaded=True)
