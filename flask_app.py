87b02e1e34af283be289d9deeabc47cf046d7006
Tunap <tunappaul@gmail.com>
2019-10-17 16:38:22 +0530

    updates

--- flask_app.py
+++ flask_app.py
@@ -25,4 +25,6 @@ def test_get(tester):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = False
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
